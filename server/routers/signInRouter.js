a7f457f0a78498df0b361d7fd2953766fb37d2ce
RessChild <39401832+RessChild@users.noreply.github.com>
2021-03-24 16:45:30 +0900

    encrypt phone_number

--- server/routers/signInRouter.js
+++ server/routers/signInRouter.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-// router.use(express.json());
+// jwt
 const { generateJwt } = require('../jwt/jwt.js');
-const userSchema = require('../schema/userSchema/userSchema');
+
+// 암호화
+const { encrypt, decrypt } = require('../crypto/aes');
 
 // sms 서비스 연결
-const { requestSMS } = require('./sms/sens');
+const { requestSMS } = require('../sms/sens');
 
-// mongodb 탐색 함수
+// mongodb
+const userSchema = require('../schema/userSchema/userSchema');
 const getUser = (phone_number) => userSchema.findOne({'phone_number': phone_number });
 
 // 로그인 및 회원가입
 router.post('/', async (req, res) => {
     const { phone_number } = req.body;
-    const user = await getUser(phone_number);
+    const aes_phone_number = encrypt(phone_number);
+    const user = await getUser(aes_phone_number);
     // console.log(result, Date.now());
 
     if (!user) { // 사용자 정보가 없는 경우
@@ -37,17 +41,20 @@ router.post('/auth-sms', async (req, res) => {
 
 // 새 계정 생성
 router.post('/new-account', async (req, res) => {
+    // AES 로 암호화
     const { phone_number } = req.body;
-    const user = await getUser(phone_number);
-    // 이미 해당 사용자가 존재함
+    const aes_phone_number = encrypt(phone_number);
+
+    const user = await getUser(aes_phone_number);
+    // 이미 해당 사용자가 존재하는 경우
     if( user ) return res.status(501).json({ "success": false }); 
 
-    // salt 는 일단 안하는걸로 하자
+    // 신규 정보 생성하여 db에 저장
     const new_user = new userSchema({
-        phone_number: phone_number,
+        phone_number: aes_phone_number,
     });
     const result = await new_user.save(); // 신규 유저 생성
-    // console.log("user result:", result);
+    console.log('new-account', result);
     res.status(200).json({ "success": true });
 });
 
